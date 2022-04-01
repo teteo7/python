73307b04159fc73ee7c09de469381340cdbd24d1
teteo7 <teteocomputer@gmail.com>
2022-04-02 05:09:19 +0900

    soft commit

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import openpyxl
-import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import filename_check0
-from filename_check0 import filecheck
+from filename_check0 import fcheck
 
 #1. chromedriver 경로 설정, Chrome이 실행됨
 driver= webdriver.Chrome('C:\chromedriver.exe')
 
-
 #2. 원하는 페이지 입력
 driver.get("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EB%82%A0%EC%94%A8")
-
 
 #이게 구방식인데 더편하다.
 # a= driver.find_element_by_xpath('//*[@id="main_pack"]/section[1]/div[1]/div[2]/div[1]/div[1]/div/div[2]/div/div[1]/div[2]/strong') #XPath경로값을 담음
@@ -20,7 +16,7 @@ driver.get("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=
 
 #이게 최신 방식인데 아직 적응 안됨. driver.find_elements()는 여러개 불러오는건가?
 #driver.find_element(By.XPATH, XPATH)의 By부분 import로 함수 가져와야함
-a= driver.find_element(By.XPATH, '//*[@id="main_pack"]/section[1]/div[1]/div[2]/div[1]/div[1]/div/div[2]/div/div[1]/div[2]/strong'); #XPath경로값을 담음
+a= driver.find_element(By.XPATH, '//*[@id="main_pack"]/section[1]/div[1]/div[2]/div[1]/div[1]/div/div[2]/div/div[1]/div[2]/strong')  #XPath경로값을 담음
 b= a.text #해당 경로에 text값 추출
 
 wb= openpyxl.Workbook()
@@ -28,11 +24,11 @@ sheet= wb.active
 print(b)
 sheet['C5'] = b
 
-output_path =filecheck()
+output_path= fcheck()
 a= str(output_path)
 print(type(a))
-print("메인출력: ", a)
+print("메인출력: ")
+print(a)
 wb.save(a)
 
 driver.close()
-
---
+++ main0.py
@@ -0,0 +1,8 @@
+from filename_check0 import fcheck
+
+print(fcheck())
+a= fcheck()
+print(a)
+
+
+
